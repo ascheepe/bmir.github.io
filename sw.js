526b2b6e99001677573fa67babcd83f9d18b8bf8
Axel Scheepers <no@spam.please>
2026-02-16 20:31:24 +0100

    re-indent these too

--- sw.js
+++ sw.js
@@ -1,24 +1,24 @@
 const CACHE_NAME = "bmi-app-v6";
 const FILES = [
-    "./",
-    "./bmir.html",
-    "./manifest.json",
-    "./sw.js",
-    "./favicon.ico",
-    "./icon-192.png",
-    "./icon-512.png"
+  "./",
+  "./bmir.html",
+  "./manifest.json",
+  "./sw.js",
+  "./favicon.ico",
+  "./icon-192.png",
+  "./icon-512.png"
 ];
 
 self.addEventListener("install", event => {
-    event.waitUntil(
-        caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
-    );
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(FILES))
+  );
 });
 
 self.addEventListener("fetch", event => {
-    event.respondWith(
-        caches.match(event.request)
-            .then(response => response || fetch(event.request))
-    );
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => response || fetch(event.request))
+  );
 });
 
